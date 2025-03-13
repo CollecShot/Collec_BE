e0e9395725d28b922833b7bf0c7a4bf9161371a0
GOOHAESEUNG <amu6675@naver.com>
2025-03-13 18:44:48 +0900

    feat : 사진 상세 조회 기능 #16

--- src/main/java/side/project/collec/photo/controller/PhotoController.java
+++ src/main/java/side/project/collec/photo/controller/PhotoController.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import side.project.collec.global.exception.customException.AlbumNotFoundException;
+import side.project.collec.global.exception.customException.PhotoNotFoundException;
 import side.project.collec.photo.domain.dto.req.PhotoRequestDto;
+import side.project.collec.photo.domain.dto.resp.PhotoResponseDto;
 import side.project.collec.photo.service.PhotoService;
 import side.project.collec.global.exception.codes.SuccessCode;
 
@@ -29,6 +31,19 @@ public class PhotoController {
                                             @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
         photoService.savePhoto(requestDto, image);
         return new ResponseEntity<>(SuccessCode.CREATED.getMessage(), SuccessCode.CREATED.getStatus());
+    }
+
+    //사진 상세 조회
+    @GetMapping("/details")
+    @ExceptionHandler(value = {PhotoNotFoundException.class})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Bad Request!"),
+            @ApiResponse(responseCode = "403", description = "Forbidden! Already Exist"),
+            @ApiResponse(responseCode = "404", description = "Photo Not Found!")  // 404 추가
+    })
+    public ResponseEntity<PhotoResponseDto> getPhoto(@RequestParam("id") Long id) throws IOException {
+        PhotoResponseDto photoResponseDto = photoService.photoDetail(id);
+        return ResponseEntity.ok(photoResponseDto);
     }
 
 }
