14f5f7148992a2869fd9a0b1a4f0396faa838318
hoyeon <hoyeonying@gmail.com>
2025-04-26 05:03:21 +0900

    refactor: User와 UserAlbum 관련한 부분 수정 #32

--- src/main/java/side/project/collec/user/repository/UserRepository.java
+++ src/main/java/side/project/collec/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package side.project.collec.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import side.project.collec.user.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByDeviceUID(String deviceUID);
+    Optional<User> findByDeviceUID(String deviceUID);
 }
