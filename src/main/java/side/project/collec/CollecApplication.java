111970e4a1cc5497df97fadba4c8b00c897681f6
hoyeon <hoyeonying@gmail.com>
2025-07-01 19:20:24 +0900

    fix: 서버 기본 시간대를 Asia/Seoul로 설정하여 시간대 불일치 문제 해결

--- src/main/java/side/project/collec/CollecApplication.java
+++ src/main/java/side/project/collec/CollecApplication.java
@@ -1,13 +1,24 @@
 package side.project.collec;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "https://collec.site", description = "콜렉 사이트")})
 public class CollecApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollecApplication.class, args);
