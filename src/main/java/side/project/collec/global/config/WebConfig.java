b60740fc53db433fe0eaad40762b9ea15447b190
hoyeon <hoyeonying@gmail.com>
2025-07-01 19:29:49 +0900

    fix: CORS 설정 개선으로 클라이언트 접근 허용 범위 및 메서드 명확화

--- src/main/java/side/project/collec/global/config/WebConfig.java
+++ src/main/java/side/project/collec/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://collec.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
